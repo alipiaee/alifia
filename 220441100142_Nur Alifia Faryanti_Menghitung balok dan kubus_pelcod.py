dbada899335024c761bacec4e97be4c9396f528d
alipiaee <113223656+alipiaee@users.noreply.github.com>
2022-09-16 22:17:56 +0700

    Add files via upload

---
+++ 220441100142_Nur Alifia Faryanti_Menghitung balok dan kubus_pelcod.py
@@ -0,0 +1,23 @@
+#nama : Nur Alifia Faryanti
+#NIM : 220441100142
+
+
+#rumus volume balok
+print("Menghitung Volume Balok")
+p = int(input("Masukkan Panjangnya:"))
+l = int(input("Masukkan Lebarnya:"))
+t = int(input("Masukkan Tingginya:"))
+
+volumebalok = p*l*t 
+
+print("Volume Baloknya Adalah:", volumebalok)
+
+#rumus volume kubus
+print("Menghitung Volume Kubus")
+s = int(input("masukkan sisi pertama:"))
+s = int(input("masukkan sisi kedua:"))
+s = int(input("masukkan sisi ketiga:"))
+
+volumekubus = s*s*s
+
+print("Volume Kubusnya Adalah:", volumekubus)
