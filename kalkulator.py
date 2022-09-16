5bcf2d483e6c342c5dbdd16a36b31e6489005c46
alipiaee <113223656+alipiaee@users.noreply.github.com>
2022-09-16 22:17:20 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,22 @@
+angka1 = 14
+angka2 = 4
+
+# membuat penjumlahan
+jumlah=angka1 + angka2
+
+# membuat pengurangan 
+kurang=angka1 - angka2
+
+# membuat perkalian
+kali=angka1 * angka2
+
+# membuat pembagian 
+bagi=angka1 / angka2 
+
+# hasil 12 ditambah 4 sama dengan = 16
+# print("Hasil 12 ditambah 4 sama dengan:", jumlah)
+# print("Hasil",angka1, "ditambah",angka2, "sama dengan:", jumlah)
+print(f"hasil {angka1} ditambah {angka2} sama dengan = {jumlah} ")
+print(f"hasil {angka1} dikurang {angka2} sama dengan = {kurang} ")
+print(f"hasil {angka1} dikali {angka2} sama dengan = {kali} ")
+print(f"hasil {angka1} dibagi {angka2} sama dengan = {bagi} ")
